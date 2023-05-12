6c30626558cd5f5e84d01d8f262c4958aed26996
Chad Angelelli <chad.angelelli@gmail.com>
2023-05-11 21:57:01 -0600

    add validation tests

--- test/potamic/db_test.clj
+++ test/potamic/db_test.clj
@@ -3,24 +3,26 @@
 
             [potamic.db :as db]))
 
+(def valid-uris
+  ["redis://localhost:6379/0"
+   "redis://USER:PASS@localhost:1234"
+   "redis://USER:PASS@localhost:12345"
+   "redis://USER:PASS@localhost:123456"
+   "redis://USER:PASS@localhost:123456/1"
+   "redis://:PASS@localhost:6379"
+   "redis://:PASS@localhost:6379/0"
+   "redis://111.222.333.444:6379/0"
+   "redis://USER:PASS@111.222.333.444:1234"
+   "redis://USER:PASS@111.222.333.444:12345"
+   "redis://USER:PASS@111.222.333.444:123456"
+   "redis://USER:PASS@111.222.333.444:123456/1"
+   "redis://:PASS@111.222.333.444:6379"
+   "redis://:PASS@111.222.333.444:6379/0"])
+
 (deftest make-conn-test
   (testing "potamic.db/make-conn"
-    (let [valid-uris ["redis://localhost:6379/0"
-                      "redis://USER:PASS@localhost:1234"
-                      "redis://USER:PASS@localhost:12345"
-                      "redis://USER:PASS@localhost:123456"
-                      "redis://USER:PASS@localhost:123456/1"
-                      "redis://:PASS@localhost:6379"
-                      "redis://:PASS@localhost:6379/0"
-                      "redis://111.222.333.444:6379/0"
-                      "redis://USER:PASS@111.222.333.444:1234"
-                      "redis://USER:PASS@111.222.333.444:12345"
-                      "redis://USER:PASS@111.222.333.444:123456"
-                      "redis://USER:PASS@111.222.333.444:123456/1"
-                      "redis://:PASS@111.222.333.444:6379"
-                      "redis://:PASS@111.222.333.444:6379/0"]]
-      (doseq [uri valid-uris
-              :let [[?conn ?err] (db/make-conn {:uri uri})]]
-        (is (nil? ?err))
-        (is (= {:spec {:uri uri} :pool {}} (assoc ?conn :pool {})))))
+    (doseq [uri valid-uris
+            :let [[?conn ?err] (db/make-conn {:uri uri})]]
+      (is (nil? ?err))
+      (is (= {:spec {:uri uri} :pool {}} (assoc ?conn :pool {}))))
     )) ; end make-conn-test
---
+++ test/potamic/validation_test.clj
@@ -0,0 +1,35 @@
+(ns potamic.validation-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [potamic.validation :as v]
+            [potamic.db-test :as db-test]))
+
+(deftest f-test
+  (testing "potamic.validation/f"
+    (let [[k err-map f] (v/f int? "Invalid integer")]
+      (is (= k :fn))
+      (is (= err-map {:error/message "Invalid integer"}))
+      (is (fn? f)))
+    )) ; end f-test
+
+(deftest invalidate-test
+  (testing "potamic.validation/invalidate"
+    (is (nil? (v/invalidate int? 1)))
+    (is (nil? (v/invalidate [:enum 1 2 3] 2)))
+    (is (nil? (v/invalidate [:map {:closed true} [:a int?] [:b string?]]
+                            {:a 52 :b "ok"})))
+    (is (= (v/invalidate [:map {:closed true} [:a int?] [:b string?]]
+                         [:my :vector])
+           ["invalid type"]))
+    (is (= (v/invalidate [:map {:closed true} [:a int?] [:b string?]]
+                         {:c 1 :d "ok"})
+           {:a ["missing required key"]
+            :b ["missing required key"]
+            :c ["disallowed key"]
+            :d ["disallowed key"]}))
+    )) ; end invalidate-test
+
+(deftest valid-redis-uri?-test
+  (testing "potamic.validation/valid-redis-uri?"
+    (doseq [uri db-test/valid-uris]
+      (is (v/valid-redis-uri? uri)))
+    )) ; end valid-redis-uri?-test
