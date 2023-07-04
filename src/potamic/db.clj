8c3da09d1d0e2a79e9a86ad6e97f5b5a05aa6c11
Chad Angelelli <chad.angelelli@gmail.com>
2023-07-04 11:42:35 -0600

    db: change return value for make-conn

--- src/potamic/db.clj
+++ src/potamic/db.clj
@@ -6,8 +6,9 @@
             [taoensso.carmine :as car]))
 
 (defn make-conn
-  "Creates a connection for Redis. Returns vector of `[?conn ?err]`.
-  On success, `?conn` will be usable by `taoensso.carmine/wcar`
+  "Creates a connection for Redis. Returns `conn` or throws Potamic Error.
+  On success, `conn` will be usable by `potamic.queue` and the underlying
+  `taoensso.carmine/wcar` library.
 
   **Examples:**
 
@@ -15,18 +16,19 @@
   (require '[potamic.db :as db])
 
   (db/make-conn :uri \"redis://localhost:6379/0\")
-  ;= [{:spec {:uri \"redis://localhost:6379/0\"}
-  ;=   :pool #taoensso.carmine.connections.ConnectionPool{..}
-  ;=  nil]
+  ;= {:spec
+  ;=  {:uri \"redis://localhost:6379/0\"}
+  ;=   :pool #taoensso.carmine.connections.ConnectionPool[..]}
   ```"
   [& opts]
   (let [{:keys [uri pool] :as args} (apply hash-map opts)]
     (if-let [args-err (v/invalidate dbv/Valid-Make-Conn-Args args)]
-    [nil
-     (e/error {:potamic/err-type :potamic/args-err
-               :potamic/err-msg (str "Invalid args provided to "
-                                     "potamic.db/make-conn")
-               :potamic/err-data {:args args :err args-err}})]
-    [{:spec {:uri uri}
-      :pool (or pool (car/connection-pool {}))}
-     nil])))
+      (let [err (e/error {:potamic/err-type :potamic/args-err
+                          :potamic/err-fatal? true
+                          :potamic/err-msg (str "Invalid args provided to "
+                                                "potamic.db/make-conn")
+                          :potamic/err-data {:args args :err args-err}})
+            ])
+
+    {:spec {:uri uri}
+     :pool (or pool (car/connection-pool {}))})))
